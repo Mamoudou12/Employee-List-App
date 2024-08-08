ddf2d1c230b3dbd4cd223aa8c996b8b120f4613c
Mamoudou12 <bmamoudou463@gmail.com>
2024-08-08 14:44:30 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div id="app">
+    <EmployeeList />
+  </div>
+</template>
+
+<script>
+import EmployeeList from './components/EmployeeList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EmployeeList
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,47 @@
+
+<template>
+  <div>
+    <ol class="employee-list">
+      <li v-for="employee in employees" :key="employee.name">
+        {{ employee.name }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmployeeList',
+  data() {
+    return {
+      employees: [
+        { name: 'Mohamed  Bankolé' },
+        { name: 'Aichettou Gaye' },
+        { name: 'Mohamed Soumaré' },
+        { name: 'Harouna Traoré' },
+        { name: 'Thillo Sem' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.employee-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.employee-list li {
+  padding: 20px;
+  margin: 5px 0;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.employee-list li:hover {
+  background-color: #f0f0f0;
+}
+
+</style>
